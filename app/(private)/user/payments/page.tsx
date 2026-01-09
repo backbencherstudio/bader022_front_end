e190713a17fa0687c56938c4bcd43a33eb2a2122
SopnilSwapon <sopnilswapon1@gmail.com>
2026-01-09 15:12:25 +0600

    added payments page in user dashboard

---
+++ app/(private)/user/payments/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PaymentHistory from "../../admin/components/payments/PaymentHistory";
+
+export default function page() {
+  return (
+    <div>
+      <PaymentHistory />
+    </div>
+  );
+}

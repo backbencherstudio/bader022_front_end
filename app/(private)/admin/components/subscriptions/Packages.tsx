bfc3f555a050c36746d04c8a6925f017325126bd
Parvez Rahman <parvezz13913@gmail.com>
2026-02-21 09:22:39 +0600

    Updated

--- app/(private)/admin/components/subscriptions/Packages.tsx
+++ app/(private)/admin/components/subscriptions/Packages.tsx
@@ -1,13 +1,6 @@
 "use client";
 
-import {
-  Edit,
-  Eye,
-  Pencil,
-  RefreshCcw,
-  SaudiRiyal,
-  Search,
-} from "lucide-react";
+import { Eye, Pencil, RefreshCcw, SaudiRiyal, Search } from "lucide-react";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
@@ -31,6 +24,7 @@ import {
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { TabsContent } from "@radix-ui/react-tabs";
 import { EditSubscriptionModal } from "./EditSubscriptionModal";
+import PackageTab from "./PackageTab";
 
 // import { PaymentDetails, PaymentModal } from "./PaymentModal";
 
@@ -178,79 +172,12 @@ export default function Packages() {
             </div>
           </div>
           <TabsContent value="packages">
-            {/* Table */}
-            <div className="overflow-hidden rounded-2xl border border-muted/40">
-              <div className="overflow-x-auto">
-                <Table>
-                  <TableHeader>
-                    <TableRow className="bg-muted/30">
-                      <TableHead className="pl-8">Package Name</TableHead>
-                      <TableHead>Business Type</TableHead>
-                      <TableHead>Duration</TableHead>
-                      <TableHead>Price</TableHead>
-                      <TableHead>Package Status</TableHead>
-                      <TableHead>Created On</TableHead>
-                      <TableHead className="pr-8">Action</TableHead>
-                    </TableRow>
-                  </TableHeader>
-
-                  <TableBody>
-                    {demoPayments.map((r) => (
-                      <TableRow key={r.id}>
-                        <TableCell className="pl-8 font-medium">
-                          {r.id}
-                        </TableCell>
-                        <TableCell>{r.merchantName}</TableCell>
-
-                        <TableCell>
-                          <div className="flex items-center gap-3">
-                            <Avatar className="h-9 w-9">
-                              <AvatarImage src={r.businessAvatar} />
-                              <AvatarFallback>
-                                {initials(r.businessName)}
-                              </AvatarFallback>
-                            </Avatar>
-                            <span className="font-medium">
-                              {r.businessName}
-                            </span>
-                          </div>
-                        </TableCell>
-
-                        <TableCell className="font-semibold">
-                          <div className="flex items-center">
-                            <SaudiRiyal size={14} />
-                            {r.amount}
-                          </div>
-                        </TableCell>
-                        <TableCell>
-                          <StatusPill status={r.status} />
-                        </TableCell>
-                        <TableCell>{r.date}</TableCell>
-
-                        <TableCell className="pr-8">
-                          <div className="flex gap-3">
-                            <button className="h-10 w-10 text-muted-foreground hover:text-black rounded-xl border hover:bg-white flex items-center justify-center cursor-pointer">
-                              <Eye className="h-5 w-5" />
-                            </button>
-                            <button className="h-10 w-10 text-muted-foreground hover:text-black rounded-xl border hover:bg-white flex items-center justify-center cursor-pointer">
-                              <Edit className="h-5 w-5" />
-                            </button>
-                          </div>
-                        </TableCell>
-                      </TableRow>
-                    ))}
-
-                    {demoPayments.length === 0 && (
-                      <TableRow>
-                        <TableCell colSpan={9} className="py-10 text-center">
-                          No payment history found.
-                        </TableCell>
-                      </TableRow>
-                    )}
-                  </TableBody>
-                </Table>
-              </div>
+            <div className="mb-4 flex items-end justify-end">
+              <button className="px-4 py-2 border text-muted-foreground rounded-lg cursor-pointer">
+                Add Plan
+              </button>
             </div>
+            <PackageTab />
           </TabsContent>
           <TabsContent value="active_subscription">
             {/* Table */}

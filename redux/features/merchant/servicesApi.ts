a7a4fcf4f5ce86afebcc6781113ae5c1d6158401
abdulbariks <abdulbarik1997m@gmail.com>
2026-03-03 15:54:17 +0600

    Update Service

--- redux/features/merchant/servicesApi.ts
+++ redux/features/merchant/servicesApi.ts
@@ -4,16 +4,29 @@ import { baseApi } from "@/redux/api/baseApi";
 
 export const servicesApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
-    //  all services
+    //  All Services
     allServices: builder.query({
       query: () => ({
         url: "/admin/service/index",
         method: "GET",
       }),
+      providesTags: ["Services"],
     }),
+
+    // Update Service
+    updateServiceById: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `/admin/service/update/${id}`,
+        method: "PUT",
+        body: data,
+      }),
+      invalidatesTags: ["Services"],
+    }),
+    
   }),
 });
 
 export const {
   useAllServicesQuery,
+  useUpdateServiceByIdMutation
 } = servicesApi;

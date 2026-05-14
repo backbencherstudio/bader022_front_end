db5191daf67fa1c88618ccea5845c93ba2e66eca
abdulbariks <abdulbarik1997m@gmail.com>
2026-05-14 11:27:49 +0600

    OrderDetailsModal language change

--- app/(private)/user/components/dashboard/OrderDetailsModal.tsx
+++ app/(private)/user/components/dashboard/OrderDetailsModal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useI18n } from "@/components/provider/I18nProvider";
 import { Button } from "@/components/ui/button";
 import { DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { useOrderDetailsQuery } from "@/redux/features/userDashboard/userDashboard";
@@ -13,6 +14,7 @@ export function OrderDetailsDialog({
   onReschedule: (serviceId: number) => void;
   onCancel: () => void;
 }) {
+   const { t, locale } = useI18n();
   const { data, isLoading, error } = useOrderDetailsQuery(booking_id!, {
     skip: !booking_id,
   });
@@ -30,7 +32,7 @@ export function OrderDetailsDialog({
   if (error || !bookingData) {
     return (
       <DialogContent className="p-6">
-        <p className="text-red-500">Failed to load order details.</p>
+        <p className="text-red-500"> {locale=='ar'?"فشل تحميل تفاصيل الطلب":"Failed to load order details."}</p>
       </DialogContent>
     );
   }
@@ -42,62 +44,62 @@ export function OrderDetailsDialog({
   // console.log(serviceId,"fddddddddddddddddddddd")
 
   return (
-    <DialogContent className="p-0 max-h-[600px] w-full overflow-y-auto rounded-2xl border border-gray-200 dark:border-gray-700 dark:bg-gray-800 shadow-sm">
+    <DialogContent className="p-0 max-h-150 w-full overflow-y-auto rounded-2xl border border-gray-200 dark:border-gray-700 dark:bg-gray-800 shadow-sm">
       {/* Header */}
       <DialogHeader className="flex flex-row items-center justify-between px-6 py-4 border-b border-border">
-        <DialogTitle className="text-base font-semibold">Order Details</DialogTitle>
+        <DialogTitle className="text-base font-semibold"> {locale=="ar"?"تفاصيل الطلب":"Order Details"}</DialogTitle>
       </DialogHeader>
 
       {/* Body */}
       <div className="px-6 py-5 space-y-6">
         {/* Merchant Information */}
         <div className="space-y-2">
-          <h3 className="text-sm font-semibold">Merchant Information</h3>
+          <h3 className="text-sm font-semibold">{locale=="ar"?"معلومات التاجر":"Merchant Information "}</h3>
           <div className="rounded-xl border border-border px-5 py-4 space-y-3 text-sm">
             <p>
-              Merchant name: <span className="font-medium">{merchant_info.merchant_name}</span>
+                {locale=="ar"?"الفرع":"Branch:"}<span className="font-medium">{merchant_info.merchant_name}</span>
             </p>
             <p>
-              Location: <span className="font-medium">{merchant_info.location}</span>
+               {locale=="ar"?"الموقع":"Location:"}<span className="font-medium">{merchant_info.location}</span>
             </p>
             <p>
-              Phone: <span className="font-medium">{merchant_info.phone}</span>
+              {locale=="ar"?"الهاتف":"Phone:"} <span className="font-medium">{merchant_info.phone}</span>
             </p>
           </div>
         </div>
 
         {/* Booking Information */}
         <div className="space-y-2">
-          <h3 className="text-sm font-semibold">Booking Information</h3>
+          <h3 className="text-sm font-semibold"> {locale=="ar"?"معلومات الحجز":"Booking Information"}</h3>
           <div className="rounded-xl border border-border px-5 py-4 space-y-4 text-sm">
             <div className="flex items-center justify-between gap-4">
-              <p>Booking ID:</p>
+              <p> {locale=="ar"?"رقم الحجز":"Booking ID:"}</p>
               <p className="font-medium text-right">{booking_info.booking_id}</p>
             </div>
             <div className="flex items-center justify-between gap-4">
-              <p>Service:</p>
+              <p> {locale=="ar"?"الخدمة":"Service:"}</p>
               <p className="font-medium text-right">{booking_info.service_name}</p>
             </div>
             <div className="flex items-center justify-between gap-4">
-              <p>Date & Time:</p>
+              <p> {locale=="ar"?"التاريخ والوقت":"Date & Time:"}</p>
               <p className="font-medium text-right">{booking_info.date_time}</p>
             </div>
             <div className="flex items-center justify-between gap-4">
-              <p>Duration:</p>
+              <p> {locale=="ar"?"المدة":"Duration:"}</p>
               <p className="font-medium text-right">{booking_info.duration} min</p>
             </div>
             <div className="flex items-center justify-between gap-4">
-              <p>Staff:</p>
+              <p> {locale=="ar"?"الموظفين":"Staff:"}</p>
               <p className="font-medium text-right">{booking_info.staff_name}</p>
             </div>
             <div className="flex items-center justify-between gap-4">
-              <p>Price:</p>
+              <p> {locale=="ar"?"السعر":"Price:"}</p>
               <p className="font-medium text-right">{booking_info.price} SAR</p>
             </div>
 
             {/* Payment Status */}
             <div className="flex items-center justify-between gap-4 pt-2">
-              <p>Payment Status:</p>
+              <p> {locale=="ar"?"حالة الدفع":"Payment Status:"}</p>
               <span className="inline-flex items-center justify-center rounded-md border border-[#22C55E] px-4 py-1 text-sm font-medium text-[#15803D]">
                 {booking_info.payment_status}
               </span>
@@ -111,7 +113,7 @@ export function OrderDetailsDialog({
             className="py-3"
             onClick={() => onReschedule(serviceId)} 
           >
-            Reschedule Appointment 
+              {locale=="ar"?"إعادة جدولة الموعد":"Reschedule Appointment"}
           </Button>
 
           <Button
@@ -119,7 +121,7 @@ export function OrderDetailsDialog({
             variant="outline"
             className="cursor-pointer py-3"
           >
-            Cancel Booking
+             {locale=="ar"?"إلغاء الحجز":"Cancel Booking"}
           </Button>
         </div>
       </div>
